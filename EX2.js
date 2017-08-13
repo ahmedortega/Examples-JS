26006580a0cd7b95899e8526dbcbd1991542e4f2
DESKTOP-4KP52D6\Ahmed Ortega <ahmedsniper2020@outlook.com>
2017-08-13 20:02:50 +0200

    Example 2 Hamza Full Screen Overlay

---
+++ EX2.js
@@ -0,0 +1,7 @@
+var overlay = document.getElementById('overlayId');
+function OpenDiv() {
+  overlay.style.transform = "scale(1)";
+}
+function CloseDiv() {
+  overlay.style.transform = "scale(0)";
+}
